ca6d247df7355a4b4d9f073bb0f1822b035dcaf3
shekhus <cpsatav@gmail.com>
2022-11-25 20:23:58 +0530

    Coneatenat String

--- client/src/App.js
+++ client/src/App.js
@@ -1,55 +1,51 @@
-import React, {useState,useEffect} from 'react';
-import {getWeb3} from './utils.js'
-import Strings from './contracts/Strings.json'
+import React, { useEffect, useState } from 'react';
+import Strings from './contracts/Strings.json';
+import { getWeb3 } from './utils.js';
 
 function App() {
-const [web3, setWeb3] =  useState(undefined);
-const [contract ,setContract] = useState (undefined);
-const [length ,setLength] = useState(unefined);
-const [concatenation,setConcatenation] =useState(unefined);
+  const [web3, setWeb3] = useState(undefined);
+  const [contract, setContract] = useState(undefined);
+  const [length, setLength] = useState(undefined);
+  const [concatenation, setConcatenation] = useState(undefined);
 
-useEffect (() =>{
-  const init = async () =>{
-const web3 =await getWeb3();
+  useEffect(() => {
+    const init = async () => {
+      const web3 = await getWeb3();
 
-const networkId = await web3.eth.net.getId();
-const deployedNetwork = Strings.networks[networkId];
-const contract = new web3.eth.Contract(
-  Strings.abi,
-  deployedNetwork && deployedNetwork.address,
-);
-//to save web3 in a contract in state of the conponent
-setWeb3(web3);
-setContract(contract);
-  } 
-  init()
- },
- []);
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = Strings.networks[networkId];
+      const contract = new web3.eth.Contract(
+        Strings.abi,
+        deployedNetwork && deployedNetwork.address,
+      );
+      setWeb3(web3);
+      setContract(contract);
+    }
+    init();
+  }, []);
 
- async function calculateLength(e){
-  e.preventDefault();
-  const length = await contract.methods
-  //access the function in smart contract i.e length 
-  //then extract the word we pass into the form (value of firts input)
-  .length(e.target.elements[0].value)
-  .call()
-  setLength(length);
- }
+  async function calculateLength(e) {
+    e.preventDefault();
+    const length = await contract.methods
+      .length(e.target.elements[0].value)
+      .call();
+    setLength(length);
+  }
 
- async function concatenate(e){
-  e.preventDefault();
-  const concatenation = await contract.methods
-  //access the function in smart contract i.e length 
-  //then extract the word we pass into the form (value of firts input)
-  .concatenate(e.target.elements[0].value,
-    e.target.elements[0].value)
-  .call()
-  setConcatenation(concatenation);
- }
+  async function concatenate(e) {
+    e.preventDefault();
+    const concatenation = await contract.methods
+      .concatenate(
+        e.target.elements[0].value, 
+        e.target.elements[1].value
+      )
+      .call();
+    setConcatenation(concatenation);
+  }
 
   if (!web3) {
     return <div>Loading...</div>;
-  } 
+  }
 
   return (
     <div className="container">
@@ -58,7 +54,7 @@ setContract(contract);
       <div className="row">
         <div className="col-sm-12">
           <h2>Length</h2>
-          <form onSubmit={e =>calculateLength(e)}>
+          <form onSubmit={e => calculateLength(e)}>
             <div className="form-group">
               <label htmlFor="string-length">String</label>
               <input type="text" className="form-control" id="string-length" />
@@ -74,7 +70,7 @@ setContract(contract);
       <div className="row">
         <div className="col-sm-12">
           <h2>Concatenate</h2>
-          <form onSubmit={e =>concatenate(e)}>>
+          <form onSubmit={e => concatenate(e)}>
             <div className="form-group">
               <label htmlFor="string1">String 1</label>
               <input type="text" className="form-control" id="string1" />
--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,29 @@
-const path = require("path");
+//https://goerli.infura.io/v3/12d81d2652574509b991c7f01ffbd77f
+const fs =require('fs');
+const HDWalletProvider= require('@truffle/hdwallet-provider')
 
-module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
-  compilers: {
-    solc: {
-      version: '0.8.0'
-    }
-  }
-};
+const secrets = JSON.parse(fs.readFileSync('.secrets').toString().trim());
+//JSON.parse(fs.readFileSync('.secrets').toString().trim());
+// truffle-hdwallet-provier
+module.exports = {networks : {
+            development: {
+          host: "127.0.0.1",
+          port: 8545,
+          network_id: "*" // Match any network id
+        },
+        
+    goerli:{
+        provider:() =>
+            new HDWalletProvider(
+                secrets.seed,
+                `https://goerli.infura.io/v3/${secrets.projectId}`
+            ),
+            network_id :5
+            }  
+          },
+          compilers: {
+            solc: {
+              version: "^0.8.0"
+            }
+          }
+        };
